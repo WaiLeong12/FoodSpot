2266b2769e68f20ed641bb6b8b936e32ecb672d3
WaiLeong <cutegtp12345@gmail.com>
2025-04-18 10:56:29 +0800

    FoodSpot

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,99 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold
+  );
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: Home', style: optionStyle),
+    Text('Index 1: Saved', style: optionStyle),
+    Text('Index 2: Post', style: optionStyle),
+    Text('Index 3: Community', style: optionStyle),
+    Text('Index 4: Me', style: optionStyle),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Row(
+            children: <Widget>[
+              Image.asset(
+                'assets/images/foodspotlogo.png',
+                width: 40,
+                height: 40,
+              ),
+              const SizedBox(width: 10),
+              const Text('FoodSpot'),
+            ],
+          ),
+        ),
+
+        body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.orange,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black87, // Darker gray for unselected items
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/images/homelogo.png', width: 40, height: 40),
+              label: 'Home',
+              backgroundColor: Colors.orange,
+            ),
+
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/images/foodspotlogo.png', width: 40, height: 40),
+              label: 'FoodSpot',
+              backgroundColor: Colors.orange,
+            ),
+
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/images/add.png', width: 40, height: 40),
+              label: 'Post',
+              backgroundColor: Colors.orange,
+            ),
+
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/images/community.png', width: 40, height: 40),
+              label: 'Community',
+              backgroundColor: Colors.orange,
+            ),
+
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/images/me.png', width: 40, height: 40),
+              label: 'Me',
+              backgroundColor: Colors.orange,
+            ),
+          ],
+        ),
       ),
-      home: HomePage(),
     );
   }
 }
